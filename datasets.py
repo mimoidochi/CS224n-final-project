5f36e54dc5adb2c1242cb280e05cd9db96f91e42
mimoidochi <tataver@gmail.com>
2024-02-25 16:46:59 -0800

    Add explicit encoding ='utf8' everywhere

--- datasets.py
+++ datasets.py
@@ -212,13 +212,13 @@ def load_multitask_data(sentiment_filename,paraphrase_filename,similarity_filena
     sentiment_data = []
     num_labels = {}
     if split == 'test':
-        with open(sentiment_filename, 'r') as fp:
+        with open(sentiment_filename, 'r', encoding='utf8') as fp:
             for record in csv.DictReader(fp,delimiter = '\t'):
                 sent = record['sentence'].lower().strip()
                 sent_id = record['id'].lower().strip()
                 sentiment_data.append((sent,sent_id))
     else:
-        with open(sentiment_filename, 'r') as fp:
+        with open(sentiment_filename, 'r', encoding='utf8') as fp:
             for record in csv.DictReader(fp,delimiter = '\t'):
                 sent = record['sentence'].lower().strip()
                 sent_id = record['id'].lower().strip()
@@ -231,7 +231,7 @@ def load_multitask_data(sentiment_filename,paraphrase_filename,similarity_filena
 
     paraphrase_data = []
     if split == 'test':
-        with open(paraphrase_filename, 'r') as fp:
+        with open(paraphrase_filename, 'r', encoding='utf8') as fp:
             for record in csv.DictReader(fp,delimiter = '\t'):
                 sent_id = record['id'].lower().strip()
                 paraphrase_data.append((preprocess_string(record['sentence1']),
@@ -239,7 +239,7 @@ def load_multitask_data(sentiment_filename,paraphrase_filename,similarity_filena
                                         sent_id))
 
     else:
-        with open(paraphrase_filename, 'r') as fp:
+        with open(paraphrase_filename, 'r', encoding='utf8') as fp:
             for record in csv.DictReader(fp,delimiter = '\t'):
                 try:
                     sent_id = record['id'].lower().strip()
@@ -253,14 +253,14 @@ def load_multitask_data(sentiment_filename,paraphrase_filename,similarity_filena
 
     similarity_data = []
     if split == 'test':
-        with open(similarity_filename, 'r') as fp:
+        with open(similarity_filename, 'r', encoding='utf8') as fp:
             for record in csv.DictReader(fp,delimiter = '\t'):
                 sent_id = record['id'].lower().strip()
                 similarity_data.append((preprocess_string(record['sentence1']),
                                         preprocess_string(record['sentence2'])
                                         ,sent_id))
     else:
-        with open(similarity_filename, 'r') as fp:
+        with open(similarity_filename, 'r', encoding='utf8') as fp:
             for record in csv.DictReader(fp,delimiter = '\t'):
                 sent_id = record['id'].lower().strip()
                 similarity_data.append((preprocess_string(record['sentence1']),
